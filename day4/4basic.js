781c2c6a827f1edc82cf0cc3d7b950a31d163d7f
rifkiandriyanto <andriyantorifki@gmail.com>
2022-02-22 20:42:51 +0700

    add day 4

---
+++ day4/4basic.js
@@ -0,0 +1,22 @@
+// myFunction = async () => {
+//   console.log('Halllo Mars');
+//   await console.log('Halllo Dunia');
+//   console.log('Halllo Jupiter');
+// };
+
+// myFunction();
+
+async function myDisplay() {
+  console.log('HI John');
+  let myPromise = new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve;
+      console.log('Hello World !!');
+    }, 3000);
+  });
+  console.log('HI Doe');
+
+  await myPromise;
+}
+
+myDisplay();
